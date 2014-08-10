4090bcad3b3d3b23c94516ecfcd6f4a0bc58e949
Robert Rees <shudderfix@gmail.com>
2014-08-10 12:18:46 +0100

    Creates a public static assets folder and brings in Bootstrap

--- models.py
+++ models.py
@@ -2,4 +2,14 @@
 from google.appengine.ext import ndb
 
 class Game(ndb.Model):
-	title: ndb.StringProperty(required=True)
+	title = ndb.StringProperty(required=True)
+	description = ndb.TextProperty()
+
+class Character(ndb.Model):
+	name = ndb.StringProperty(required=True)
+	description = ndb.TextProperty()
+
+class Message(ndb.Model):
+	dt_sent = ndb.DateTimeProperty(required=True)
+	dt_receive = ndb.DateTimeProperty(required=True)
+	message = ndb.TextProperty(required=True)
