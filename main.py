e323d077d7e9243a6f94fee4b7613e7edde92b26
Robert Rees <shudderfix@gmail.com>
2014-08-17 11:19:01 +0100

    Creates the flow required to create a new game

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import jinja2
 
 from google.appengine.api import users
 
+import models
+
 templates = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
     extensions=['jinja2.ext.autoescape'],
@@ -14,13 +16,34 @@ templates = jinja2.Environment(
 class HomePage(webapp2.RequestHandler):
 	def get(self):
 
+		user = users.get_current_user()
+
 		template_values = {
-			"user" : users.get_current_user()
+			"user" : user,
+			"games_created" : models.Game.query(models.Game.creator == user)
 		}
 
 		template = templates.get_template('home.html')
 		self.response.write(template.render(template_values))
 
+class NewGameHandler(webapp2.RequestHandler):
+	def post(self):
+		for key in ['title', 'description']:
+			if not key in self.request.POST.keys():
+				webapp2.abort(400, 'Missing key {key}'.format(key=key))
+
+		user = users.get_current_user()
+
+		title = self.request.POST['title']
+		description = self.request.POST['description']
+
+		new_game = models.Game(title=title, description=description, creator=user, admins=[user], players=[user])
+		new_game.put()
+
+
+		return webapp2.redirect('/home')
+
 app = webapp2.WSGIApplication([
 	webapp2.Route(r'/home', handler=HomePage),
+	webapp2.Route(r'/game/new', handler=NewGameHandler)
 	], debug=True)
